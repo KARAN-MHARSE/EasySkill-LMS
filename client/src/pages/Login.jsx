2a5bfa932893e5a115f7e2c93c38f7e762c0fc28
Karan Mharse <krnmharse06@gmail.com>
2024-07-02 21:01:53 +0530

    added authentication backend functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,47 @@
-import {Link} from 'react-router-dom'
-import { FaGoogle } from "react-icons/fa";
+import {Link, useNavigate} from 'react-router-dom'
+import { FaGoogle, FaPassport } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
-
+import { useState } from 'react';
+import {signInStart,signInSuccess,signInFailure} from './Redux/User/user.reducers'
+import {useDispatch} from 'react-redux'
 
 function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch()
+  const [formData,setFormData] = useState({
+    email:"",
+    password:""
+  })
+
+  const handleChange = (e)=>{
+    setFormData({...formData,[e.target.id] : e.target.value})
+  }
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+
+    dispatch(signInStart())
+    
+
+    const res = await fetch("http://localhost:5000/api/v2/auth/sign-in",{
+      method:"POST",
+      credentials: "include",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(formData)
+    })
+    const data = await res.json();
+    if(data.success){
+      dispatch(signInSuccess(data.user))
+      navigate('/')
+    }else{
+      dispatch(signInFailure())
+      alert(data.message)
+    }
+  }
+
   return (
     <div className='h-screen flex gap-8 font-openSans bg-primaryGrey pr-10'>
       <img className='h-full sm:w-[60%] object-cover' fetchpriority="high" decoding="async"  src="https://neurosciencenews.com/files/2023/10/ai-walking-robot-neurosicnes.jpg"></img>
@@ -17,9 +54,9 @@ function Login() {
 
         <h2 className='text-[18px] font-bold'>START YOUR JOURNEY</h2>
         <h1 className='text-3xl font-bold mb-5'>Hi,Welcome Back!</h1>
-        <form className='flex flex-col gap-3'>
-          <input className='border-2 border-zinc-500 rounded-[7px] px-2 py-1' type="email" name="" id="" placeholder='Email' />
-          <input className='border-2 border-zinc-500 rounded-[7px] px-2 py-1' type="password" name="" id="" placeholder='Password' />
+        <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
+          <input  className='border-2 border-zinc-500 rounded-[7px] px-2 py-1' onChange={handleChange} type="email" name="" id="email" placeholder='Email' />
+          <input className='border-2 border-zinc-500 rounded-[7px] px-2 py-1' onChange={handleChange} type="password" name="" id="password" placeholder='Password' />
           <p className='text-sm text-primaryBlue font-semibold'>Forgot your password?</p>
           <button className='text-white bg-primaryBlue rounded-[7px] px-2 py-1'>Sign In</button>
         </form>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,45 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import { useState } from 'react';
 
 
 function Register() {
+  const navigate = useNavigate();
+  const [formData,setFormData] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    password:""
+  })
+  console.log(formData)
+
+  const handleChange = (e)=>{
+    setFormData({...formData,[e.target.id] : e.target.value})
+  }
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    
+
+    const res = await fetch("http://localhost:5000/api/v2/auth/sign-up",{
+      method:"POST",
+      credentials: "include",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(formData)
+    })
+    const data = await res.json();
+    console.log(data)
+    if(data.success){
+      navigate('/sign-in')
+    }else{
+      alert(data.message)
+    }
+  }
+
   return (
     <div className='h-screen flex gap-8 font-openSans bg-primaryGrey pr-10'>
     <img className='h-full sm:w-[60%] object-cover' fetchpriority="high" decoding="async"  src="https://neurosciencenews.com/files/2023/10/ai-walking-robot-neurosicnes.jpg"></img>
@@ -18,13 +53,13 @@ function Register() {
 
       <h2 className='text-[18px] font-bold'>REGISTER NOW</h2>
       <h1 className='text-3xl font-bold mb-5'>Welcome To The LMS</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <div className='w-full flex justify-between gap-3'>
-          <input className='border-2 border-zinc-500 rounded-[7px] px-2 py-1 w-1/2' type="email" name="" id="" placeholder='First Name' />
-          <input className='border-2 border-zinc-500 rounded-[7px] px-2 py-1 w-1/2' type="email" name="" id="" placeholder='Last Name' />
+          <input onChange={handleChange} className='border-2 border-zinc-500 rounded-[7px] px-2 py-1 w-1/2' type="text" name="" id="firstName" placeholder='First Name' />
+          <input onChange={handleChange} className='border-2 border-zinc-500 rounded-[7px] px-2 py-1 w-1/2' type="text" name="" id="lastName" placeholder='Last Name' />
         </div>
-        <input className='border-2 border-zinc-500 rounded-[7px] px-2 py-1' type="email" name="" id="" placeholder='Email' />
-        <input className='border-2 border-zinc-500 rounded-[7px] px-2 py-1' type="password" name="" id="" placeholder='Password' />
+        <input onChange={handleChange} className='border-2 border-zinc-500 rounded-[7px] px-2 py-1' type="email" name="" id="email" placeholder='Email' />
+        <input onChange={handleChange} className='border-2 border-zinc-500 rounded-[7px] px-2 py-1' type="password" name="" id="password" placeholder='Password' />
         <div className='flex items-center gap-2'>
           <input type="checkbox" name="" id="" />
           <p className='text-sm text-zinc-400 font-semibold'>I agree to the processing of personal data</p>
